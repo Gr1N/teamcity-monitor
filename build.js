1677c755fabc1ea381c90123471cf6d339207d77
Nikita Grishko <grin.minsk@gmail.com>
2015-03-14 23:04:44 +0300

    Add webpack.config

--- build.js
+++ build.js
@@ -1,22 +1,4 @@
-var path = require('path'),
-    webpack = require('webpack'),
-
-    config = {
-        context: path.join(__dirname, 'static/js'),
-        entry: './index.js',
-        output: {
-            path: path.join(__dirname, 'static/assets')
-        },
-        module: {
-            loaders: [{
-                test: /packery/,
-                loader: 'imports?define=>false&this=>window'
-            }]
-        },
-        plugins: [
-            new webpack.optimize.UglifyJsPlugin(),
-            new webpack.optimize.DedupePlugin()
-        ]
-    };
+var webpack = require('webpack'),
+    config = require('./webpack.config');
 
 webpack(config).run(function() {});
